c52a02548f490b6c3f726732f68ec56aedbc46dd
Aleksa24 <aleksaivanovic24444@gmail.com>
2020-11-21 22:51:10 +0100

    added tests for BeerController

--- src/main/java/cloud/springframework/gurukursmsccbeerservice/web/controller/BeerController.java
+++ src/main/java/cloud/springframework/gurukursmsccbeerservice/web/controller/BeerController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
-    @GetMapping({"{/beerId}"})
+    @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
 
         //todo impl
@@ -24,7 +24,7 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @PutMapping("{/beerId}")
+    @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
